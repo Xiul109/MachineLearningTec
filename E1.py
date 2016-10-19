3f4a115852550d96191725794b6a698428243f0f
Iván García <ciberpatro@gmail.com>
2016-10-19 16:20:10 +0200

    Fixed the reading of^Cpanish characters

--- E1.py
+++ E1.py
@@ -3,17 +3,20 @@
 #Luis Cabañero Gómez
 #Iván García Pulido
 
-import codecs
-xml=codecs.open('IncidenciasTDTHist.xml', "r",encoding='utf-8', errors='ignore')
-rows=xml.read().split('<raiz>')[1].replace('</raiz>','').split('<incidenciaGeolocalizada>')[1:]
-xml.close()
-cuenta=0
-fichero=open('salida.xml','w')
-fichero.close()
-fichero=open('salida.xml','a')
-for row in rows:
-	if '<provincia>BIZKAIA' in row:
-		cuenta+=1
-		fichero.write('<incidenciaGeolocalizada>'+row+'\n')
-fichero.close()
-print("Registers: "+str(cuenta))
+import sys
+if len(sys.argv) is not 2:
+	print ("Usage: python3 E1.py <file>") 
+else:
+	xml=open(sys.argv[1],"r", encoding="latin-1")
+	rows=xml.read().split('<raiz>')[1].replace('</raiz>','').split('<incidenciaGeolocalizada>')[1:]
+	xml.close()
+	cuenta=0
+	fichero=open('salida.xml','w')
+	fichero.close()
+	fichero=open('salida.xml','a')
+	for row in rows:
+		if '<provincia>BIZKAIA' in row:
+			cuenta+=1
+			fichero.write('<incidenciaGeolocalizada>'+row+'\n')
+	fichero.close()
+	print("Registers: "+str(cuenta))
