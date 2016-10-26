81975f2dbaa87984e3242a4983176ea9c0bf9c34
xiul <xiul10901@gmail.com>
2016-10-26 10:11:00 +0200

    E4: Modified Script and added Spectral and EM clustering

--- E4.py
+++ E4.py
@@ -6,81 +6,105 @@ import matplotlib.pyplot as plt
 import sys
 import numpy as np
 
+#Plot function
+def plot(sil, ks, y_xsL, X, name):
+	bestSil=max(sil)
+	i=sil.index(bestSil)
+	k=ks[i]
+	y_xs=y_xsL[i]
+	print(name)
+	print('Clusters: '+str(k))
+	print('Silhouette: %0.3f'%bestSil)
+	
+	
+	plt.scatter(ks, sil,c='blue', marker='o', s=20,)
+	plt.plot(ks, sil)
+	plt.savefig('figure4-'+name+'-sil.png', dpi=200)
+	plt.clf()
+	
+	colors=['b','g','r','c','m','y','k','w']
+	markers=['o','v','^','+','x','8','s','p','*','h','D']
+	nMark=len(markers)
+	nCol=len(colors)
+	for i in range(k):
+		plt.scatter(X[y_xs==i,0], X[y_xs==i,1],c=colors[i%nCol], marker=markers[(i//nCol)%nMark], s=15, label='cluster '+str(i))
+	
+	plt.savefig('figure4-'+name+'-clusts.png', dpi=1000)
+	plt.clf()
+
 # Obtain coordinates
-if(len(sys.argv) is not 2):
-	print("Usage: python3 E4.py <file>")
-	exit()
-else:
-	xml=open(sys.argv[1],'r',encoding='latin-1')
-	rows=xml.read().split('<incidenciaGeolocalizada>')[1:]
-	latLen=len('<latitud>')
-	lonLen=len('<longitud>')
-	X=[]
-	nElements=0
-	for row in rows:
-		if('<tipo>Accidente' in row):
-			Xaux=[]
-			Xaux.append(float(row[row.find('<longitud>')+lonLen:row.find('</longitud>')]))
-			Xaux.append(float(row[row.find('<latitud>')+latLen:row.find('</latitud>')]))
-			
-			X.append(Xaux)
-			nElements+=1
-	print("Accidents: "+str(nElements))
-	X=np.array(X)
-	xml.close()
+def dataExtraction():
+	if(len(sys.argv) is not 2):
+		print("Usage: python3 E4.py <file>")
+		exit()
+	else:
+		xml=open(sys.argv[1],'r',encoding='latin-1')
+		rows=xml.read().split('<incidenciaGeolocalizada>')[1:]
+		latLen=len('<latitud>')
+		lonLen=len('<longitud>')
+		X=[]
+		nElements=0
+		for row in rows:
+			if('<tipo>Accidente' in row):
+				Xaux=[]
+				Xaux.append(float(row[row.find('<longitud>')+lonLen:row.find('</longitud>')]))
+				Xaux.append(float(row[row.find('<latitud>')+latLen:row.find('</latitud>')]))
+				
+				X.append(Xaux)
+				nElements+=1
+		print("Accidents: "+str(nElements))
+		print()
+		X=np.array(X)
+		xml.close()
+	return X
 
+#Program Execution
+def algorithmExecution(algF,X, metrics, k1, k2, steps, name):
+	
+	y_algL= []
+	
+	ks=[]
+	sil=[]
+	for k in range(k1,k2,steps):
+		alg=algF(k)
+		y_alg=alg.fit_predict(X)
+		y_algL.append(y_alg)
+		# Silhouette coefficient
+		ks.append(k)
+		sil.append(metrics.silhouette_score(X, y_alg))
+	
+	return sil, ks, y_algL
 
-##KMeans
+X=dataExtraction()
 
-#Clustering execution
+k1=20
+k2=41
+steps=2
+
+#KMeans
 from sklearn.cluster import KMeans
 from sklearn import metrics
 
-k1=20
-k2=40
-steps=2
 iterations=10
 max_iter=50
 tol=1e-04
 random_state=0
-y_kmL= []
 
-ks=[]
-sil=[]
-for k in range(k1,k2,steps):
-	km = KMeans(k, 'random', n_init = iterations, max_iter=max_iter, tol=tol,random_state=random_state)
-	y_km=km.fit_predict(X)
-	y_kmL.append(y_km)
-	# Silhouette coefficient
-	ks.append(k)
-	sil.append(metrics.silhouette_score(X, y_km))
+sil,ks, y=algorithmExecution(lambda k:KMeans(k, 'random', n_init = iterations, max_iter=max_iter, tol=tol,random_state=random_state),X, metrics,k1,k2,steps,'KMeans')
+plot(sil, ks, y, X, 'KMeans')
 
-#Plot results
-bestSil=max(sil)
-i=sil.index(bestSil)
-k=ks[i]
-y_km=y_kmL[i]
-print('Clusters: '+str(k))
-print('Silhouette: %0.3f'%bestSil)
+#EM
+from sklearn.mixture import GMM
+covar_type='tied'
 
-plt.scatter(ks, sil,c='blue', marker='o', s=20,)
-plt.plot(ks, sil)
-plt.show()
+sil,ks, y=algorithmExecution(lambda k:GMM(n_components=k, covariance_type=covar_type, init_params='wc', n_iter=20),X, metrics,k1,k2,steps,'EM')
+plot(sil, ks, y, X, 'EM')
 
-colors=['b','g','r','c','m','y','k','w']
-markers=['o','v','^','+','x','8','s','p','*','h','D']
-nMark=len(markers)
-nCol=len(colors)
-for i in range(k):
-	plt.scatter(X[y_km==i,0], X[y_km==i,1],c=colors[i%nCol], marker=markers[(i//nCol)%nMark], s=15, label='cluster '+str(i))
+#Spectral
+from sklearn.cluster import SpectralClustering
 
-plt.savefig('figure4-KMeans.png', dpi=1000)
-
-##EM
-
-
-
-##Spectral
+sil,ks, y=algorithmExecution(lambda k: SpectralClustering(n_clusters=k, eigen_solver='arpack', affinity="nearest_neighbors"), X, metrics,k1,k2,steps,'Spectral')
+plot(sil, ks, y, X, 'Spectral')
 
 
 
