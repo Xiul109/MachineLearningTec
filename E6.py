e459d941bacffc52220a603c615d504a0691ccc0
Iván García <ciberpatro@gmail.com>
2017-01-02 13:37:55 +0100

    Added comments

--- E6.py
+++ E6.py
@@ -5,39 +5,45 @@ import csv
 from sklearn import preprocessing
 import sklearn.neighbors
 from scipy import cluster
-
+#Args checking
 if(len(sys.argv) is not 3):
 	print("Usage: python3 E6.py <input_file> <output_file>")
 	exit()
+#File open
 csv_file=csv.reader(open(sys.argv[1]))
 aux=list(csv_file)
-
+#Get values
 fields=aux[0]
 data=aux[1:]
-numpyData=list(map(lambda x : list(map(float,x[1:])), aux[1:]))
+numpyData=list(map(lambda x : list(map(float,x[1:])), data))
 
+#Normalize data
 min_max_scaler = preprocessing.MinMaxScaler()
 numpyData = min_max_scaler.fit_transform(numpyData)
 
-
-#metrics=['euclidean', 'manhattan', 'chebyshev','minkowski','wminkowski','seuclidean','mahalanobis']
-#methods=['complete', 'average', 'weighted','single','ward','median','centroid']
+#Selecting parameters for the clustering 
+##metrics=['euclidean', 'manhattan', 'chebyshev','minkowski','wminkowski','seuclidean','mahalanobis']
+##methods=['complete', 'average', 'weighted','single','ward','median','centroid']
 metric='manhattan'
 method='complete'
 threshold=42
 
+
+#Hierarchy clustering
 dist = sklearn.neighbors.DistanceMetric.get_metric(metric)
 matsim = dist.pairwise(numpyData)
 avSim = np.average(matsim)
 
 clusters = cluster.hierarchy.linkage(matsim, method = method)
+#Plot clustering
 cluster.hierarchy.dendrogram(clusters, color_threshold=threshold)
 plt.title(metric+" "+method)
 #plt.savefig(me+" "+m, dpi=1000)
 plt.show()
 
+#Cut dendrogram
 clusters=cluster.hierarchy.fcluster(clusters,threshold,criterion='distance')
-summary=[]
+#Zone with text label
 from statistics import mean
 
 for i in range(len(data)):
@@ -52,6 +58,7 @@ for i in clust:
 	print("Cluster %d: Number of accidents mean %f;	Number of roads mean %f"%(i,accidentsMean,roadsMean))
 
 accMeans.sort()
+print(accMeans)
 labels=['low','medium','high']
 
 for i in range(len(data)):
@@ -59,6 +66,7 @@ for i in range(len(data)):
 		if data[i][len(data[i])-1]==accMeans[j][1]:
 			data[i][len(data[i])-1]=labels[j]
 
+#Save file
 with open(sys.argv[2],'w') as outfile:
 	writer=csv.writer(outfile)
 	fields.append('dangerLevel')
